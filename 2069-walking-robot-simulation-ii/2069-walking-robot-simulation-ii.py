966cbd24dc4bd2063b321232f8515e22df9fcf79
Sbhargava <109794944+sanchitbhargava@users.noreply.github.com>
2026-04-07 13:02:36 +0530

    Time: 53 ms (72.65%), Space: 23.5 MB (84.62%) - LeetHub

---
+++ 2069-walking-robot-simulation-ii/2069-walking-robot-simulation-ii.py
@@ -0,0 +1,62 @@
+from typing import List
+
+class Robot:
+
+    def __init__(self, width: int, height: int):
+        self.w = width
+        self.h = height
+        self.x = 0
+        self.y = 0
+        self.dir = 0  
+        self.cycle = 2 * (width + height - 2)
+
+    def step(self, num: int) -> None:
+        num %= self.cycle
+
+        
+        if num == 0:
+            if self.x == 0 and self.y == 0:
+                self.dir = 3  
+            return
+
+        while num > 0:
+            if self.dir == 0:  
+                move = min(num, self.w - 1 - self.x)
+                self.x += move
+                num -= move
+                if num > 0:
+                    self.dir = 1
+
+            elif self.dir == 1:
+                move = min(num, self.h - 1 - self.y)
+                self.y += move
+                num -= move
+                if num > 0:
+                    self.dir = 2
+
+            elif self.dir == 2:
+                move = min(num, self.x)
+                self.x -= move
+                num -= move
+                if num > 0:
+                    self.dir = 3
+
+            else: 
+                move = min(num, self.y)
+                self.y -= move
+                num -= move
+                if num > 0:
+                    self.dir = 0
+
+    def getPos(self) -> List[int]:
+        return [self.x, self.y]
+
+    def getDir(self) -> str:
+      return ["East", "North", "West", "South"][self.dir]
+
+
+# Your Robot object will be instantiated and called as such:
+# obj = Robot(width, height)
+# obj.step(num)
+# param_2 = obj.getPos()
+# param_3 = obj.getDir()
